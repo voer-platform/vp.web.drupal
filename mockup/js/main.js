36edd04b6ee3e548052e2069b9d916d23f67abcf
Huy Vu <vuquochuy@gmail.com>
2012-12-28 22:57:42 +0700

    Update mockup

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -20,5 +20,21 @@
         $nav.removeClass('subnav-fixed')
       }
     }
+
+    var $window = $(window);
+
+    // Disable certain links in docs
+    $('section [href^=#]').click(function (e) {
+      e.preventDefault()
+    });
+
+     // side bar
+    $('.voer-sidenav').affix({
+      offset: {
+        top: function () { return $window.width() <= 980 ? 290 : 210 }
+        , bottom: 270
+      }
+    });
+
   })
-}(window.jQuery)
+}(window.jQuery);
