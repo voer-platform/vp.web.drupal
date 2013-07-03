b9185a198fd1a82ecc57705d500518ed813bab1f
Huy Vu <vuquochuy@gmail.com>
2013-07-03 21:49:16 +0700

    Update person

--- sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
+++ sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
@@ -2,12 +2,12 @@
   <div class="vertical-shadow suggested-action">
     <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo drupal_get_path('module', 'voer_blocks') ?>/images/default_author.jpg');"><a href="/author/<?php print $id ?>">&nbsp;</a></div>
     <h2 class="pulls ellipsis suggested-action-title">
-      <?php echo l($fullname, 'author/' . $id); ?>
+      <?php echo l($fullname, 'person/' . $id); ?>
     </h2>
 
     <h5>&nbsp;</h5>
 
-    <?php if ($text) : ?>
+    <?php if (isset($text)) : ?>
       <p><?php print $text; ?></p>
     <?php endif; ?>
 
