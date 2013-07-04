d7d480ca15cbce873aa356af403fbf52bb16ce0d
Luong Trieu <luongtd151@yahoo.com>
2013-07-04 14:58:00 +0700

    update 5 blocks in homepage

--- sites/all/modules/voer/voer_blocks/templates/block--voer-module.tpl.php
+++ sites/all/modules/voer/voer_blocks/templates/block--voer-module.tpl.php
@@ -1,6 +1,6 @@
 <div class="span4">
   <div class="vertical-shadow suggested-action">
-    <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo drupal_get_path('module', 'voer_blocks') ?>/images/default_image.jpg');"><a href="/module/<?php print $material_id ?>">&nbsp;</a></div>
+    <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo $image_path; ?>');"><a href="/module/<?php print $material_id ?>">&nbsp;</a></div>
     <h2 class="pulls ellipsis suggested-action-title">
       <?php echo l($title, 'module/' . $material_id); ?>
     </h2>
