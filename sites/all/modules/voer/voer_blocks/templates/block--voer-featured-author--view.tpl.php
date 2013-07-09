0d66c6a19961dd6e6bceda19cfc2334d1c1015f7
Huy Vu <vuquochuy@gmail.com>
2013-07-10 02:57:37 +0700

    Display personal/id Show amount beside category name

--- sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
+++ sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
@@ -2,7 +2,7 @@
   <div class="vertical-shadow suggested-action">
     <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo drupal_get_path('module', 'voer_blocks') ?>/images/default_author.jpg');"><a href="/person/<?php print $id ?>">&nbsp;</a></div>
     <h2 class="pulls ellipsis suggested-action-title">
-      <?php echo l($fullname, 'person/' . $id); ?>
+      <?php echo l($fullname, 'person/' . $person_id); ?>
     </h2>
 
     <h5>&nbsp;</h5>
