73afd5d2a5f1ed1ce76ee6900518c35704f304d7
Luong Trieu <luongtd151@yahoo.com>
2013-07-09 21:26:03 +0700

    change variable name (bug: variable $id has been replace when pass into template)

--- sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
+++ sites/all/modules/voer/voer_blocks/templates/block--voer-featured-author--view.tpl.php
@@ -1,8 +1,8 @@
 <div class="span4">
   <div class="vertical-shadow suggested-action">
-    <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo drupal_get_path('module', 'voer_blocks') ?>/images/default_author.jpg');"><a href="/person/<?php print $id ?>">&nbsp;</a></div>
+    <div class="suggested-action-image-link cls_module" style="background-image: url('<?php echo drupal_get_path('module', 'voer_blocks') ?>/images/default_author.jpg');"><a href="/person/<?php print $author_id ?>">&nbsp;</a></div>
     <h2 class="pulls ellipsis suggested-action-title">
-      <?php echo l($fullname, 'person/' . $id); ?>
+      <?php echo l($fullname, 'person/' . $author_id); ?>
     </h2>
 
     <h5>&nbsp;</h5>
