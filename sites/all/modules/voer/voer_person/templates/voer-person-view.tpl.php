36f6efbaa94fb02259927f2df5ba4b98039320c7
Huy Vu <vuquochuy@gmail.com>
2013-07-11 00:23:58 +0700

    Add voer_person module

---
+++ sites/all/modules/voer/voer_person/templates/voer-person-view.tpl.php
@@ -0,0 +1,4 @@
+<section style="overflow: hidden;">
+  <img src="<?php echo base_path() . drupal_get_path('module', 'voer_person') . '/images/author.png'; ?>" width="120" style="float: right" />
+  <h2><?php print $fullname; ?></h2>
+</section>
