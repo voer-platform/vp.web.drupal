de8ef0c8a5450e14dcfe59cfb1e0f702eec52b04
Huy Vu <vuquochuy@gmail.com>
2013-07-13 00:08:00 +0700

    Re-sort contron on form Fix trim text when import from doc

--- sites/all/themes/bootstrap/templates/html.tpl.php
+++ sites/all/themes/bootstrap/templates/html.tpl.php
@@ -6,7 +6,6 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-  <?php print $scripts; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -16,5 +15,6 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <?php print $scripts; ?>
 </body>
 </html>
