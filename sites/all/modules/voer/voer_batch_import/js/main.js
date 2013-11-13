678124a04ebc5acd18377104de3434e91b4d89a0
Huy Vu <vuquochuy@gmail.com>
2013-11-14 01:11:25 +0700

    Fix upload mass file

--- sites/all/modules/voer/voer_batch_import/js/main.js
+++ sites/all/modules/voer/voer_batch_import/js/main.js
@@ -5,6 +5,8 @@
                 'auto'     : false,
                 'removeCompleted' : false,
                 'formData' : {'uid': Drupal.settings.voer_import.uid},
+                'queueSizeLimit' : 10,
+                'multi'    : true,
                 'fileTypeDesc' : 'Document Files',
                 'fileTypeExts' : '*.doc; *.docx',
                 'swf'      : '/sites/all/modules/voer/voer_batch_import/uploadify/uploadify.swf',
