00c009e3ada8f0071c0a2cfed0e7cbbcda2ba6ac
Huy Vu <vuquochuy@gmail.com>
2013-11-23 15:10:20 +0700

    Style for browse all page

--- sites/all/modules/voer/voer_search/templates/browse-all-page.tpl.php
+++ sites/all/modules/voer/voer_search/templates/browse-all-page.tpl.php
@@ -1,5 +1,5 @@
-<div class="container">
-<div class="sidebar col-span-3">
+<div class="row">
+<div class="sidebar span3">
   <div class="row">
     <h4 class="col-span-6"> Materials (<span id="total_movies">250</span>)</h4>
     <div class="col-span-6 progress">
@@ -27,10 +27,12 @@
     </div>
   </div>
 </div>
-<div class="movies content col-span-9" id="materials">
+<div class="movies content span9">
+  <div class="row-fluid show-grid" id="materials">
+  </div>
 </div>
 <script id="template" type="text/html">
-  <div class="col-span-4 movie">
+  <div class="span4 movie">
     <div class="thumbnail">
       <div class="caption">
         <h4><a href="/m/{{material_id}}">{{title}}</a></h4>
