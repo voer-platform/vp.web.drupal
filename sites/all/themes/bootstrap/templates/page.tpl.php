1aa0077de894916a68dff1628f593f4f23e62b63
Huy Vu <vuquochuy@gmail.com>
2013-12-12 00:59:03 +0700

    Change new style

--- sites/all/themes/bootstrap/templates/page.tpl.php
+++ sites/all/themes/bootstrap/templates/page.tpl.php
@@ -54,8 +54,12 @@
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
-
-    <?php print render($page['header']); ?>
+    <?php if (!empty($page['header'])): ?>
+      <?php print render($page['header']); ?>
+      <div class="mainpage-browser clear">
+        <a class="btn-browser-all left clear">Browser All</a>
+      </div>
+    <?php endif; ?>
   </header> <!-- /#header -->
 
   <div class="row">
